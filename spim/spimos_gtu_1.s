557e936c6cc55d9ea2932fa58ec05cb0a6fdb7c5
SelmanOzleyen <selman.ozleyen2017@gtu.edu.tr>
2020-06-11 16:51:18 +0300

    spimos

--- spim/spimos_gtu_1.s
+++ spim/spimos_gtu_1.s
@@ -6,34 +6,34 @@ prompt: .asciiz "\nExitting Kernel...\n"
 .text
 .globl main
 main:
-li $t0,-1
-#fork
-li $v0,18
-syscall
-beq $v0,$0,if_child
+    li $t0,-1
+    #fork
+    li $v0,18
+    syscall
+    beq $v0,$0,if_child
 if_child_end:
-li $v0,18
-syscall
-beq $v0,$0,if_child2
+    li $v0,18
+    syscall
+    beq $v0,$0,if_child2
 if_child_end2:
-li $v0,18
-syscall
-beq $v0,$0,if_child3
+    li $v0,18
+    syscall
+    beq $v0,$0,if_child3
 if_child_end3:
 
 l1:
-li $v0,20
-syscall
-beq $t0,$v0,finished
+    li $v0,20
+    syscall
+    beq $t0,$v0,finished
 j l1
 
 finished:
-li $v0,4
-la $a0,prompt
-syscall
-#Exit
-li $v0,22
-syscall
+    li $v0,4
+    la $a0,prompt
+    syscall
+    #Exit
+    li $v0,22
+    syscall
 
 if_child:
     la $v0,19
--- spim/spimos_gtu_2.s
+++ spim/spimos_gtu_2.s
@@ -6,51 +6,51 @@ prompt: .asciiz "\nExitting Kernel...\n"
 .text
 .globl main
 main:
-li $t0,-1
-li $t1,1
-li $t2,2
+    li $t0,-1
+    li $t1,1
+    li $t2,2
 
-li $a0, 3  #Max bound.
-li $v0, 21  #Random Number Syscall.
-syscall
-la $a0,($v0)
+    li $a0, 3  #Max bound.
+    li $v0, 21  #Random Number Syscall.
+    syscall
+    la $a0,($v0)
 
-#Get in one of those
-beq $a0,$0,load_f1
-beq $a0,$t1,load_f2
-beq $a0,$t2,load_f3
+    #Get in one of those
+    beq $a0,$0,load_f1
+    beq $a0,$t1,load_f2
+    beq $a0,$t2,load_f3
 
 load_f1_end:
 load_f2_end:
 load_f3_end:
 
 
-li $t3,10
+    li $t3,10
 l2:
-beq $t3,$0,end_l2
-#fork
-li $v0,18
-syscall
-#if it is the child
-beq $v0,$0,if_child
+    beq $t3,$0,end_l2
+    #fork
+    li $v0,18
+    syscall
+    #if it is the child
+    beq $v0,$0,if_child
 if_child_end:
-addi $t3, $t3,-1			# $t3 = $t3 -1
-j l2
+    addi $t3, $t3,-1			# $t3 = $t3 -1
+    j l2
 end_l2:
 
 l1:
-li $v0,20
-syscall
-beq $t0,$v0,finished
-j l1
+    li $v0,20
+    syscall
+    beq $t0,$v0,finished
+    j l1
 
 finished:
-li $v0,4
-la $a0,prompt
-syscall
-#Exit
-li $v0,22
-syscall
+    li $v0,4
+    la $a0,prompt
+    syscall
+    #Exit
+    li $v0,22
+    syscall
 
 load_f1:
     la $a0,filename
